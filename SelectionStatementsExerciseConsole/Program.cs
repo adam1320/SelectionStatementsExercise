0d4b0fbbf8964ed4bc0a275ee12eabc5eff3d5e3
Adam Brannon <adam1320@rocketmail.com>
2022-01-14 14:18:03 -0500

    initial commit

---
+++ SelectionStatementsExerciseConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SelectionStatementsExerciseConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var favoriteNumber = 7;
+
+            Console.WriteLine("Try to guess my favorite number. Pick a number between 1 and 10");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput > favoriteNumber) 
+            {
+                Console.WriteLine("Too high! Better luck next time!");
+            }
+            else if (userInput < favoriteNumber) 
+            {
+                Console.WriteLine("Too low! Better luck next time!");
+            }
+            else if (userInput == favoriteNumber) 
+            {
+                Console.WriteLine("You are correct! Lucky guess!");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind.");
+            }
+            
+
+            
+
+
+        }
+    }
+}
